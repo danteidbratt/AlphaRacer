76dd4ca8b01a953eb277a5376ca468ea315bf3d2
danteidbratt <danteidbratt@users.noreply.github.com>
2018-02-18 16:59:53 +0100

    ok

--- AlphaRacer.java
+++ AlphaRacer.java
@@ -59,7 +59,6 @@ public class AlphaRacer implements ActionListener{
             frame.showStats(repository.getStats());
         }
         else if (e.getSource() == frame.changeUserButton) {
-            System.out.println("eyy");
             playerInitials = frame.changeUser();
         }
     }
